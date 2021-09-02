1b26dd003d56acd8d1ad79be78c8c64747492754
Fz <lfernandozv@gmail.com>
2021-09-02 14:03:02 -0500

    cambio 2

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={{alignItems:'center'}}>
+        <Text>Grid con React</Text>
+      </View>
+      <View style={{flexDirection:'row',flexWrap:'wrap'}}>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'coral'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'skyblue'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'khaki'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'pink'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'purple'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'powderblue'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'aqua'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'maroon'}}></View>
+        <View style={{width:100,height:100,margin:10,backgroundColor:'peru'}}></View>
+      </View>
+      </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
